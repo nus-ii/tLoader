0163ccbf1c2b61e7195ef942caad84fdb243eaa3
nus-ii <nusii86@gmail.com>
2017-04-22 19:12:54 +0300

    Установлены события сборки для разворачивания  приложения. установлена зависимость путей поиска дополнительных файлов. Добавлена проверка подключения устройства.

--- DateFormat/DateFormat/Program.cs
+++ DateFormat/DateFormat/Program.cs
@@ -16,13 +16,13 @@ namespace DateFormat
         static void Main(string[] args)
         {
 
-
             var profile = BookProfile.GetProfile(BookProfileType.T2);
+            String dbCardFileName = "";
+            string dbReaderFileName = "";
+            WaitReaderConnection(profile,ref dbCardFileName,ref dbReaderFileName);
 
-            String dbFileName = GetFilePath(profile,true);
-            
-            string basePath = @"C:\MyTempBook\";
-            string logPath = string.Concat(basePath, "Log.txt");
+            string basePath = Environment.CurrentDirectory;// @"C:\MyTempBook\";
+            string logPath = Path.Combine(basePath, "Log.txt");
             var log = File.ReadAllLines(logPath);
             List<string> logList = GetList(log);
 
@@ -31,68 +31,87 @@ namespace DateFormat
             SQLiteCommand m_sqlCmd;
             m_dbConn = new SQLiteConnection();
             m_sqlCmd = new SQLiteCommand();
-            m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+            m_dbConn = new SQLiteConnection("Data Source=" + dbCardFileName + ";Version=3;");
             m_dbConn.Open();
             m_sqlCmd.Connection = m_dbConn;
             var sqlQuery = "SELECT * FROM annotation";
             SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
             DataTable dTable = new DataTable();
-            adapter.Fill(dTable); 
-            #endregion            
-            
+            adapter.Fill(dTable);
+            #endregion
+
             List<AnnotationItem> AnnotationList = new List<AnnotationItem>();
 
             foreach (DataRow r in dTable.Rows)
             {
                 var item = AnnotationItem.GetItem(r, profile);
                 AnnotationList.Add(item);
-                Console.WriteLine(item.ToCSVstring());    
+                Console.WriteLine(item.ToCSVstring());
             }
 
             List<string> anList = GetListAn(AnnotationList);
             var res = GetSource(anList, ref logList);
 
             var dn = DateTime.Now;
-            string ps = string.Format("{0}_{1}_{2}_{3}-{4}", dn.Day, dn.Month, dn.Year, dn.Hour, dn.Minute);
-            //string[] res = GetRes(sourceList, ps);
-            File.WriteAllLines(basePath + ps + ".txt", res);
+            string ps = string.Format("{0}_{1}_{2}_{3}-{4}.txt", dn.Day, dn.Month, dn.Year, dn.Hour, dn.Minute);
+            string resultPath = Path.Combine(basePath,ps);
+            File.WriteAllLines(resultPath, res);
             File.WriteAllLines(logPath, GetArray(logList));
             Console.WriteLine("All done!!!");
             Console.ReadLine();
 
         }
-
+        private static void WaitReaderConnection(BookProfile profile, ref string dbA,ref string dbB)
+        {
+            while(!GetFilePath(profile,ref dbA,ref dbB))
+            {
+                Console.Clear();
+                Console.WriteLine("Reader not found!");
+                System.Threading.Thread.Sleep(250);
+            }
+            Console.Clear();
+            Console.WriteLine("Reader ready for work, press any key!");
+            Console.ReadLine();
+        }
         private static List<string> GetListAn(List<AnnotationItem> annotationList)
         {
             List<string> result = new List<string>();
-            foreach(var i in annotationList)
+            foreach (var i in annotationList)
             {
                 result.Add(i.ToString());
             }
             return result;
         }
 
-        private static string GetFilePath(BookProfile profile,bool card=true)
+        private static bool GetFilePath(BookProfile profile, ref string card, ref string reader)
         {
             DriveInfo[] allDrives = DriveInfo.GetDrives();
             List<DriveInfo> pretendent = new List<DriveInfo>();
 
-            foreach(var d in allDrives)
+            foreach (var d in allDrives)
             {
-                if(File.Exists(Path.Combine(d.RootDirectory.FullName,profile.bookDbPath)))
-                    {
+                if (File.Exists(Path.Combine(d.RootDirectory.FullName, profile.bookDbPath)))
+                {
                     pretendent.Add(d);
-                    }
+                }
             }
 
-            DriveInfo firstPretendent = pretendent.FirstOrDefault(p => p.VolumeLabel != "READER");
+            if (pretendent.Count != 0)
+            {
+                DriveInfo cardPretendent = pretendent.FirstOrDefault(p => p.VolumeLabel != "READER");
+                DriveInfo readerPretendent = pretendent.FirstOrDefault(p => p.VolumeLabel == "READER");
 
-            if(card==false)
-                firstPretendent = pretendent.FirstOrDefault(p => p.VolumeLabel == "READER");
-
-            string result = Path.Combine(firstPretendent.RootDirectory.FullName, profile.bookDbPath);
-            return result;
+                card = Path.Combine(cardPretendent.RootDirectory.FullName, profile.bookDbPath);
+                reader= Path.Combine(readerPretendent.RootDirectory.FullName, profile.bookDbPath);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
+
+
 
         private static string[] GetArray(List<string> logList)
         {
